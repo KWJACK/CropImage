00958770be7960cf2a3c746267b2eb3be26b4b56
KWJACK <dndudwp12@gmail.com>
2017-07-04 18:04:36 +0900

    에러 케이스 추가

--- Test/BMPclass.cpp
+++ Test/BMPclass.cpp
@@ -2,6 +2,13 @@
 #include "BMPclass.h"
 
 #define WIDTHBYTES(bits) (((bits)+31)/32*4)
+#define T (0.03f)
+
+BMPclass::~BMPclass(void)
+{
+	if(m_pucBMP) delete m_pucBMP;
+
+}
 
 BMPclass::BMPclass(UINT m_height, UINT m_width, UCHAR* m_inImg):
 	m_pucBMP(nullptr)	
@@ -10,6 +17,9 @@ BMPclass::BMPclass(UINT m_height, UINT m_width, UCHAR* m_inImg):
 	if(align ==4)align=0;
 
 	int bmpWidth = (m_width + 3) / 4 * 4;	
+
+	m_uiHeight = m_height;
+	m_uiWidth = m_width;
 	
 	memset(&fh, 0, sizeof(BITMAPFILEHEADER));
 	memset(&ih, 0, sizeof(BITMAPINFOHEADER));
@@ -27,7 +37,6 @@ BMPclass::BMPclass(UINT m_height, UINT m_width, UCHAR* m_inImg):
 	ih.biSizeImage = bmpWidth * m_height;	
 	ih.biXPelsPerMeter = 0;
 	ih.biYPelsPerMeter = 0;
-
 	for(int i = 0; i < 256; i++)
 	{
 		rgb[i].rgbBlue = i;
@@ -35,10 +44,16 @@ BMPclass::BMPclass(UINT m_height, UINT m_width, UCHAR* m_inImg):
 		rgb[i].rgbRed = i;
 		rgb[i].rgbReserved = 0;
 	}
-   
+
    //이미지 데이터 복사	
 	m_pucBMP = new UCHAR[ih.biSizeImage];
 	memset(m_pucBMP, 0, sizeof(UCHAR)*ih.biSizeImage);
+
+
+	//나중에 1bit으로 바꿔야합니다. 현재 8bit
+	m_bin = new UCHAR[ih.biSizeImage];
+	memset(m_bin, 0, sizeof(UCHAR)*ih.biSizeImage);
+
 	int index = 0, oldindex=0;
 	UINT R,G,B,GRAY;	
 	for(int i=0; i<m_height; i++)
@@ -52,11 +67,83 @@ BMPclass::BMPclass(UINT m_height, UINT m_width, UCHAR* m_inImg):
 			m_pucBMP[oldindex++] = GRAY;
 		}	
 	}
+
+	//adaptiveThreshold(m_pucBMP, m_bin);
 }
 
 
-BMPclass::~BMPclass(void)
+
+void BMPclass::adaptiveThreshold(unsigned char* input, unsigned char* bin)
 {
-	if(m_pucBMP) delete m_pucBMP;
+	unsigned long* integralImg = 0;
+	int i, j;
+	long sum=0;
+	int count=0;
+	int index;
+	int x1, y1, x2, y2;
+	int S = (m_uiWidth/8);
+	int s2 = S/2;
+
+	// create the integral image
+	integralImg = (unsigned long*)malloc(m_uiWidth*m_uiHeight*sizeof(unsigned long*));
+
+	for (i=0; i<m_uiWidth; i++)
+	{
+		// reset this column sum
+		sum = 0;
+
+		for (j=0; j<m_uiHeight; j++)
+		{
+			index = j*m_uiWidth+i;
+
+			sum += input[index];
+			if (i==0)
+				integralImg[index] = sum;
+			else
+				integralImg[index] = integralImg[index-1] + sum;
+		}
+	}
+
+	// perform thresholding
+	for (i=0; i<m_uiWidth; i++)
+	{
+		for (j=0; j<m_uiHeight; j++)
+		{
+			index = j*m_uiWidth+i;
+
+			// set the SxS region
+			x1=i-s2; x2=i+s2;
+			y1=j-s2; y2=j+s2;
+
+			// check the border
+			if (x1 < 0) x1 = 0;
+			if (x2 >= m_uiWidth) x2 = m_uiWidth-1;
+			if (y1 < 0) y1 = 0;
+			if (y2 >= m_uiHeight) y2 = m_uiHeight-1;
+			
+			count = (x2-x1)*(y2-y1);
+
+			// I(x,y)=s(x2,y2)-s(x1,y2)-s(x2,y1)+s(x1,x1)
+			sum = integralImg[y2*m_uiWidth+x2] -
+				  integralImg[y1*m_uiWidth+x2] -
+				  integralImg[y2*m_uiWidth+x1] +
+				  integralImg[y1*m_uiWidth+x1];
+
+			if ((long)(input[index]*count) < (long)(sum*(1.0-T)))
+				bin[index] = 0;
+			else
+				bin[index] = 255;
+		}
+	}
+
+	free (integralImg);
+
+	for(int i=0; i<ih.biSizeImage; i++)
+	{	
+		m_pucBMP[i]= bin[i];
+	}
 
 }
+
+
+
--- Test/BMPclass.h
+++ Test/BMPclass.h
@@ -2,14 +2,16 @@
 class BMPclass
 {
 public:
-	BMPclass(UINT m_height, UINT m_width, UCHAR* m_inImg);
-	~BMPclass(void);
-
 	BITMAPFILEHEADER fh;
 	BITMAPINFOHEADER ih;
 	RGBQUAD rgb[256];
 
 	UCHAR*	m_pucBMP;	
+	UCHAR*	m_bin;	//나중에 바꿔야합니다. (8bit 인데 1bit으로)
 	UINT m_uiWidth;
 	UINT m_uiHeight;
+
+	BMPclass(UINT m_height, UINT m_width, UCHAR* m_inImg);
+	~BMPclass(void);
+	void adaptiveThreshold(unsigned char* input, unsigned char* bin);	
 };
